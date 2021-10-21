1a22a67b92e85d51f4b09a583a8cd6557c9c937a
yggit <nateriverm@gmail.com>
2021-10-21 15:42:46 +0900

    http_set 3

--- http_set.sh
+++ http_set.sh
@@ -6,3 +6,8 @@ sudo wget -P /web https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
 sudo wget -P /web https://downloads.apache.org/apr/apr-1.7.0.tar.gz
 sudo wget -P /web https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
 sudo wget -P /web https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz
+cd /web
+sudo tar xvfz httpd-2.4.51.tar.gz
+sudo tar xvfz apr-1.7.0.tar.gz
+sudo tar xvfz apr-util-1.6.1.tar.gz
+sudo tar xvfz pcre-8.45.tar.gz
